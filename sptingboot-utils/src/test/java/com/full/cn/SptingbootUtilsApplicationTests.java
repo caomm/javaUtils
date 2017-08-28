5deff758321afce305096d2a9cb11458dc850243
caomm <cao_manman@126.com>
2017-08-28 12:10:14 +0800

    备注文档

--- sptingboot-utils/src/test/java/com/full/cn/SptingbootUtilsApplicationTests.java
+++ sptingboot-utils/src/test/java/com/full/cn/SptingbootUtilsApplicationTests.java
@@ -8,6 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.*;
+
 /*@RunWith(SpringRunner.class)
 @SpringBootTest*/
 public class SptingbootUtilsApplicationTests {
@@ -23,6 +25,30 @@ public class SptingbootUtilsApplicationTests {
 			e.printStackTrace();
 		} finally {
 			xl.invoke("Quit", new Variant[] {});
+		}
+	}
+
+	/**
+	 * 从数组中获取重复的数据被统计重复出现的次数
+	 */
+	@Test
+	public void getNum(){
+		String [] arry = {"1","2","3","1","1","2","5","2","6","6","4","4","3","3","4"};
+		Map<String,Integer> map = new HashMap<String, Integer>();
+		for(int i =0 ;i<arry.length;i++){
+			if(null!= map.get(arry[i])){
+				map.put(arry[i], map.get(arry[i])+1); //value+1
+			}else{
+				map.put(arry[i],1);
+			}
+		}
+
+		Iterator it = map.entrySet().iterator();
+		while(it.hasNext()){
+			Map.Entry entry = (Map.Entry) it.next();
+			String  key  =  entry.getKey().toString();
+			int  value  =  Integer.parseInt(entry.getValue().toString());
+			System.out.println("key is :"+key+"---value :"+value);
 		}
 	}
 
