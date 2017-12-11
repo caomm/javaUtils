33712d910c034594f185bc30ca1e5c61f58e5eb0
caomm <cao_manman@126.com>
2017-12-11 10:09:03 +0800

    1、ansj分词功能研究及其测试 2、pdf在线预览添加胴动态水印功能

--- sptingboot-utils/src/main/java/com/full/cn/utils/PdfAddwaterMark.java
+++ sptingboot-utils/src/main/java/com/full/cn/utils/PdfAddwaterMark.java
@@ -24,7 +24,7 @@ public class PdfAddwaterMark {
     public static void main(String[] args) throws DocumentException,
             IOException {
         // 要输出的pdf文件
-        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File("E:\\text\\QSG_16.pdf")));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File("E:\\text\\QSG_162.pdf")));
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         // 将pdf文件先加水印然后输出
@@ -43,30 +43,33 @@ public class PdfAddwaterMark {
         BaseFont base = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H",BaseFont.EMBEDDED);
         PdfGState gs = new PdfGState();
         for (int i = 1; i < total; i++) {
-            content = stamper.getOverContent(i);// 在内容上方加水印
-            //content = stamper.getUnderContent(i);//在内容下方加水印
-            gs.setFillOpacity(0.2f);
-            // content.setGState(gs);
-            content.beginText();
-            content.setColorFill(BaseColor.LIGHT_GRAY);
-            content.setFontAndSize(base, 50);
-            content.setTextMatrix(70, 200);
-            content.showTextAligned(Element.ALIGN_CENTER, "公司内部文件，请注意保密！", 300,350, 55);
-            //Image image = Image.getInstance("E:\\text\\IMG_3862.JPG");
+            for (int j = 0; j < 10; j++) {
+                content = stamper.getOverContent(i);// 在内容上方加水印
+                //content = stamper.getUnderContent(i);//在内容下方加水印
+                gs.setFillOpacity(0.2f);
+                // content.setGState(gs);
+                content.beginText();
+                content.setColorFill(BaseColor.LIGHT_GRAY);
+                content.setFontAndSize(base, 50);
+                content.setTextMatrix(70, 200);
+                content.showTextAligned(Element.ALIGN_CENTER, "公司内部文件，请注意保密！", 300,350, 55);
+                //Image image = Image.getInstance("E:\\text\\IMG_3862.JPG");
             /*img.setAlignment(Image.LEFT | Image.TEXTWRAP);
             img.setBorder(Image.BOX);
             img.setBorderWidth(10);
             img.setBorderColor(BaseColor.WHITE);
             img.scaleToFit(1000, 72);//大小
             img.setRotationDegrees(-30);//旋转 */
-            //image.setAbsolutePosition(200, 206); // set the first background image of the absolute
-            //image.scaleToFit(200,200);
-            //content.addImage(image);
-            content.setColorFill(BaseColor.BLACK);
-            content.setFontAndSize(base, 8);
-            content.showTextAligned(Element.ALIGN_CENTER, "下载时间："
-                    + waterMarkName + "", 300, 10, 0);
-            content.endText();
+                //image.setAbsolutePosition(200, 206); // set the first background image of the absolute
+                //image.scaleToFit(200,200);
+                //content.addImage(image);
+                content.setColorFill(BaseColor.BLACK);
+                content.setFontAndSize(base, 8);
+                content.showTextAligned(Element.ALIGN_CENTER, "下载时间："
+                        + waterMarkName + "", 300, 10, 0);
+                content.endText();
+            }
+
 
 
         }
